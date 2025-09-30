5da665334f84dd6661c433cde42a424030267b60
Marine <marinefurlan@hotmail.fr>
2025-09-30 15:33:34 +0200

    comment

--- modules/ssm/main.tf
+++ modules/ssm/main.tf
@@ -1,3 +1,6 @@
+### === IAM ROLE FOR SSM ACCESS === ###
+/* Creates a role that EC2 instances can assume to communicate
+ with AWS Systems Manager (SSM).*/
 resource "aws_iam_role" "ssm_role" {
   name = "${var.name}-${var.role_name}"
 
@@ -15,12 +18,21 @@ resource "aws_iam_role" "ssm_role" {
   tags = { Name = var.role_name }
 }
 
+
+### === ATTACH MANAGED SSM POLICY === ###
+/* Grants the role permissions to use core SSM features :
+      - Session Manager for remote instance management
+      - Run Command for executing scripts
+      - Inventory and patching features*/
 resource "aws_iam_role_policy_attachment" "ssm_managed" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-  role       = aws_iam_role.ssm_role.name
+  role       = aws_iam_role.ssm_role.name                                      // To which role this policy must be attached
 }
 
+
+### === IAM INSTANCE PROFILE === ###
+// Required to attach the IAM role to EC2 instances.
 resource "aws_iam_instance_profile" "ssm_instance_profile" {
   name = "${var.name}-${var.role_name}-instance-profile"
-  role = aws_iam_role.ssm_role.name
+  role = aws_iam_role.ssm_role.name                                            // Which role is attached to this instance profile
 }
