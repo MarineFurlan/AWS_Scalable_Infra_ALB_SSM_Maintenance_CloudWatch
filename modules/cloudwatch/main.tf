702affdf26705ecd4aed8c92a9493c5ab067b629
MarineFurlan <34574120+MarineFurlan@users.noreply.github.com>
2025-09-30 16:07:34 +0200

    Update main.tf
    correct

--- modules/cloudwatch/main.tf
+++ modules/cloudwatch/main.tf
@@ -6,17 +6,17 @@ resource "aws_cloudwatch_metric_alarm" "alb_4xx_alarm" {
   metric_name         = "HTTPCode_Target_4XX_Count"
   namespace           = "AWS/ApplicationELB"
 
-  // Aggregate 4xx responses each minute
+  // Aggregates 4xx responses each minute
   evaluation_periods  = 1
   period              = 60
   statistic           = "Sum"
 
-  // Trigger alarm if 10 or more 4xx errors in one period. This value is low here for testing purposes, it would be higher in a production environment.
+  // Triggers alarm if 10 or more 4xx errors in one period. This value is low here for testing purposes, it would be higher in a production environment.
   comparison_operator = "GreaterThanOrEqualToThreshold"
   threshold           = 10
 
-  dimensions = { LoadBalancer = var.alb_arn_suffix }                 // Specify which ALB to monitor
-  alarm_actions = [aws_sns_topic.alerts.arn]                         // When alarm is triggered : sends mail via SNS
+  dimensions = { LoadBalancer = var.alb_arn_suffix }                 // Specify which ALB to monitor.
+  alarm_actions = [aws_sns_topic.alerts.arn]                         // When alarm is triggered : sends email via SNS.
 }
 
 
